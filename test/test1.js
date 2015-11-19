f72a5225008ea0d4518e742fda8a90c816b0113e
pigate <mimchiang@gmail.com>
2015-11-19 10:38:15 -0800

    wraparound writes

--- test/test1.js
+++ test/test1.js
@@ -11,4 +11,22 @@ module.exports = {
     cb.write('hi');
     assert.equal('hi', cb.read());
   },
+  'test cbuffer#WriteRead': function(){
+    var cb = gcb(2);
+    cb.write('hi1');
+    cb.write('hi2');
+    assert.equal('hi1', cb.read());
+    assert.equal('hi2', cb.read());
+    cb.write('hi3');
+    assert.equal('hi3', cb.read());
+  },
+  'test cbuffer#OverFlow': function(){
+    console.log("Testing Overflow");
+    var cb = gcb(2);
+    cb.write('1');
+    cb.write('2');
+    cb.write('3');
+    assert.equal('2', cb.read());
+    assert.equal('3', cb.read());
+  },
 }
